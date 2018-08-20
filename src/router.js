df7031346c1d08ffb453a3a81418cc72e43aee97
zsw <1050259219@qq.com>
2018-08-20 22:53:45 +0800

    base

---
+++ src/router.js
@@ -0,0 +1,13 @@
+// base
+import React, {Component} from 'react';
+import { HashRouter,Route } from 'react-router-dom';
+
+// component
+
+export default class Router extends Component {
+	render () {
+		return (
+			<HashRouter></HashRouter>
+		);
+	}
+}
